ec7356373481b49d221be4979ed983cbc42c1c9c
csaszarhunor <csaszarhunor@gmail.com>
2016-04-05 23:13:43 +0200

    gitignore

---
+++ CreateClass2/Person.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreateClass2
+{
+    class Person
+    {
+        private string name;
+        private DateTime birthDate;
+        private Gender gender;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public DateTime BirthDate
+        {
+            get { return birthDate; }
+            set { birthDate = value; }
+        }
+
+        public Person(){}
+        public Person(string name, DateTime birthDate)
+        {
+            this.Name = name;
+            this.BirthDate = birthDate;
+        }
+
+        public override string ToString()
+        {
+            return (String.Format("name: {0}, birth date: {1}", this.name, this.birthDate));
+        }
+
+    }
+
+    public enum Gender : int { Male, Female };
+}
